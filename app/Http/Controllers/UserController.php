03e244a6269c6aac2b5e80a7e6dd2aaabbc594de
Bruningbr <brunoandradeoliveira40@gmail.com>
2025-10-10 13:14:04 -0300

    Aula 10/10

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function store(Request $request){
+        // 'name',
+        // 'email',
+        // 'password',
+        // 'user_type'
+
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => $request->password,
+        'user_type' => $request->user_type
+
+    ]);
+
+    return response()->json([
+    'msg' => 'Cadastro',
+    'dados' => $user
+    ]);
+    
+    }
+    
+    // Listar todos os registros da tabela
+    public function index(){
+    $users = User::all();
+
+    return response()->json($users);
+   }
+
+   // Listar usuario po ID
+   public function show($id){
+    $user = User::find($id);
+
+    if(!$user){
+        return response()->json([
+            'msg' => 'Usuario não encontrado'
+        ]);
+    }
+
+    return response()->json($user);
+
+   }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('usuarios', [UserController::class, 'store']);    
+Route::get('usuarios', [UserController::class, 'index']);
